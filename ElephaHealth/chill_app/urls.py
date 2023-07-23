8f4541e89f91cf4207941784b56860f1d508ad95
MurenMurenus <59960529+MurenMurenus@users.noreply.github.com>
2023-07-23 12:36:36 +0700

    Update urls.py

--- ElephaHealth/chill_app/urls.py
+++ ElephaHealth/chill_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('profile/', views.UserProfileDetailView.as_view(), name="profile"),
-    path('profile/<int:pk>/', views.PkUserProfileDetailView.as_view(), name="pk_profile"),
+    path('profile/<int:pk>/', views.AdminUserProfileDetailView.as_view(), name="pk_profile"),
     path("all-profiles/", views.UserProfileListCreateView.as_view(), name="all-profiles"),
     path("research/", views.GetProfileStatistics.as_view({'get': 'list'}), name="research")
 ]
