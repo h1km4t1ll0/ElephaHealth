80fe0d116940d62b06269e056cfda6ad584e4cb1
MurenMurenus <maximkosmos5360@gmail.com>
2023-08-15 22:52:08 +0700

    Now we can edit profiles

--- ElephaHealth/chill_app/urls.py
+++ ElephaHealth/chill_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('profile/', views.UserProfileDetailView.as_view(), name="profile"),
     path('profile/<int:pk>/', views.AdminUserProfileDetailView.as_view(), name="pk_profile"),
+    path('profile_edit', views.UserProfileEdit.as_view({'post': 'retrieve'})),
     path("research/", views.GetProfileStatistics.as_view({'get': 'list'}), name="research"),
     path("getfile/<str:filename>/", views.GetFile.as_view({'get': 'list'}), name="getfile"),
 ]
